738589ad5530d34d3541e69d7628dc5bb61114c3
Sami <samitimsina05@gmail.com>
2020-06-25 21:49:44 -0500

    glfw window

--- Lightning/src/Lightning/Core/Application.cpp
+++ Lightning/src/Lightning/Core/Application.cpp
@@ -3,9 +3,32 @@
 #include "Application.h"
 
 namespace Lightning {
-	Application::Application(std::string name) {
-		Window window = Window(name);
+	Application::Application(std::string title, int width, int height) {
+		this->width = width;
+		this->height = height;
+		this->title = title;
 
+		if (!glfwInit()) { LN_CORE_FATAL("Failed to initalize GLFW"); }
+
+		LN_CORE_INFO("Creating window ({0}x{1}) \"{2}\"", width, height, title);
+		nativeWindow = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+		if (!nativeWindow) { LN_CORE_FATAL("Failed to create GLFW window"); }
+
+		glfwMakeContextCurrent(nativeWindow);
+		glfwSetWindowUserPointer(nativeWindow, this);
+		glViewport(0, 0, width, height);
+
+		SetVsync(true);
+
+		glfwSetWindowCloseCallback(nativeWindow, [](GLFWwindow* window) {
+			Application* application = (Application*)glfwGetWindowUserPointer(window);
+			application->WindowCloseCallback();
+		});
+
+		glfwSetWindowSizeCallback(nativeWindow, [](GLFWwindow* window, int width, int height) {
+			Application* application = (Application*)glfwGetWindowUserPointer(window);
+			application->WindowResizeCallback(width, height);
+		});
 	}
 
 	Application::~Application() {
@@ -13,6 +36,22 @@ namespace Lightning {
 	}
 
 	void Application::Run() {
-		while (true);
+		while (running) {
+			glfwPollEvents();
+			glfwSwapBuffers(nativeWindow);
+		}
+	}
+
+	void Application::WindowCloseCallback() {
+		running = false;
+	}
+
+	void Application::WindowResizeCallback(int width, int height) {
+		glViewport(0, 0, width, height);
+	}
+
+	void Application::SetVsync(bool enabled) {
+		vsync = enabled;
+		glfwSwapInterval(enabled ? 1 : 0);
 	}
 }
--- Lightning/src/Lightning/Core/Application.h
+++ Lightning/src/Lightning/Core/Application.h
@@ -2,16 +2,38 @@
 
 #include "lnpch.h"
 
+#include "GLFW/glfw3.h"
 #include "Base.h"
-#include "Window.h"
 
 namespace Lightning {
 	class LN_API Application {
 	public:
-		Application(std::string title = "Lightning Application");
+		Application(std::string title = "Lightning Application", int width = 1280, int height = 720);
 		virtual ~Application();
 
+		void SetVsync(bool enabled);
+
+		GLFWwindow* GetNativeWindow() { return nativeWindow; }
+		int GetWidth() { return width; }
+		int GetHeight() { return height; }
+		std::string GetTitle() { return title; }
+		bool IsVsync() { return vsync; }
+		bool IsRunning() { return running; }
+		bool IsFullscreen() { return fullscreen; }
+
 		void Run();
+	private:
+		GLFWwindow* nativeWindow;
+
+		int width, height;
+		std::string title;
+
+		bool vsync = false;
+		bool fullscreen = false;
+		bool running = true;
+
+		void WindowCloseCallback();
+		void WindowResizeCallback(int width, int height);
 	};
 	Application* CreateApplication();
 }
